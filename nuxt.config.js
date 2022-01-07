9546b904ce3b7b6806ba9bcad7a34eb6009f90e8
Justin Lung <justin.lung@hva.nl>
2022-01-07 12:43:39 +0100

    removed gsap

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,8 +18,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     script: [
-      {
-        src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js'}
+      
     ]
   },
 
@@ -30,7 +29,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vue-intersect.js', mode: 'client' }
+    
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components

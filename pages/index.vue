27fba2cc54a77412297fa93d648b229306279979
JustinLung <justinlung77@gmail.com>
2022-05-05 10:59:00 +0200

    Improved Code Structure

--- pages/index.vue
+++ pages/index.vue
@@ -8,13 +8,10 @@
 </template>
 
 <script>
-import Home from "../components/Home.vue";
-import Works from "~/components/Works.vue";
 export default {
   transition: {
     name: "layout",
     mode: "out-in",
   },
-  components: { Home, Works },
 };
 </script>

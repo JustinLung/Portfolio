adb97dd6403fa8e737eef2d0ab0ec33ab4ad5658
JustinLung <justinlung77@gmail.com>
2023-06-11 21:08:58 +0200

    .

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,23 @@
+<section>
+	<h1>About Me</h1>
+</section>
+
+<style>
+	section {
+		height: 100svh;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+	}
+
+	h1 {
+		font-family: 'Migra';
+		font-size: clamp(1rem, 4vw, 20rem);
+		font-weight: 100;
+		text-align: center;
+		max-width: 55rem;
+		padding: 0;
+		margin: 0;
+	}
+</style>

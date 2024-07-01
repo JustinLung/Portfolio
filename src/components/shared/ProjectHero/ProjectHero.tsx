b30fcf6de7b7f07ed2a4bb5fe36be4c3e5e00b0b
JustinLung <justinlung77@gmail.com>
2024-07-01 16:26:11 +0200

    fixed some styling

--- src/components/shared/ProjectHero/ProjectHero.tsx
+++ src/components/shared/ProjectHero/ProjectHero.tsx
@@ -6,6 +6,7 @@ import { ProjectRecord } from "@lib/generated/sdk";
 0;
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { easeOutExpo } from "@/utils/transitions";
 
 interface ProjectHeroProps {
   data: ProjectRecord;
@@ -15,8 +16,22 @@ export function ProjectHero(props: ProjectHeroProps) {
   const { data } = props;
   return (
     <section>
-      <div className={s["hero-wrapper"]}>
-        <h1 className={cn(s.title, "container")}>{data?.title}</h1>
+      <div className={cn(s["hero-wrapper"])}>
+        <div className={cn("container", s['title-container'])}>
+          <motion.h1
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 100 }}
+            transition={{
+              ease: easeOutExpo,
+              delay: 0.5,
+              duration: 0.3,
+            }}
+            className={cn(s.title)}
+          >
+            {data?.title}
+          </motion.h1>
+        </div>
         <Parallax className={s["hero-container"]}>
           <Image
             src={data?.image?.url as string}
